6bc6897233b0a6e08f7c50f3a77b1dad989aa432
Shruti Misal <shruti.private10@gmail.com>
2025-11-06 20:09:52 +0530

    slection sort

---
+++ sortingAlgos/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n ){
+    for(int i=0; i<n-1; i++){
+        int smallestIndex = i;
+        for(int j = i+1 ; j<n; j++){
+            if(arr[j] < arr[smallestIndex]){
+                smallestIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[smallestIndex];
+        arr[smallestIndex] = temp;
+    }
+}
+
+void printArr(int arr[],int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int n = 5;
+    int arr[] = {5,3,4,2,1};
+    selection_sort(arr,n);
+    printArr(arr,n);
+    return 0;
+}
