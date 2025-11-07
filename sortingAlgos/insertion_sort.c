958e8159f7b895f8c5a0f0b202c961e9b5250c71
Shruti Misal <shruti.private10@gmail.com>
2025-11-07 10:59:50 +0530

    insertionsort

---
+++ sortingAlgos/insertion_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void insertion_sort(int arr[],int n){ // Time complexity : O (n^2)F
+   for(int i=1; i<n; i++){
+    int curr = arr[i];
+    int prev = i-1;
+
+    while(prev>=0 && arr[prev] > curr){
+        arr[prev+1] = arr[prev];
+        prev--;
+    }
+    arr[prev+1] = curr; // placing the curr element in its correct position.
+   }
+}
+
+void printArr(int arr[] , int n){
+    for(int i=0 ; i<n; i++){
+        printf("%d " , arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n = 5;
+    int arr[] = {5,3,4,2,1};
+    insertion_sort(arr,n);
+    printArr(arr,n);
+    return 0;
+}
